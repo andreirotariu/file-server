d35d7674ee5462c6b5332c4c3a0043d4a4970e57
randrei <rotariu.c.andrei@gmail.com>
2020-05-21 14:28:18 +0300

    initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO billionairestata (first_name, last_name, career) VALUES
+  ('Gigi', 'Dangote', 'Billionaire Industrialist'),
+  ('Mihaita', 'Gates', 'Billionaire Tech Entrepreneur'),
+  ('Vasilica', 'Alakija', 'Billionaire Oil Magnate');
